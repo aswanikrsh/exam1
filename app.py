13608482cbf66bc4213e70a67f6a7cb566e636cb
Aswanikrishna C <aswanikrishnac@gmail.com>
2025-02-08 16:50:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+
+import pickle
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.tree import DecisionTreeClassifier
+
+filename = 'lin_reg.pkl'
+with open(filename, 'rb') as file:
+    loaded_model = pickle.load(file)
+
+st.title('Penguin Species Prediction')
+st.subheader('Enter the data')
+
+data = sns.load_dataset('penguins')
+# df = pd.read_csv('data')
+
+uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+
+    prediction = loaded_model.predict(data)
+    st.write(prediction)
